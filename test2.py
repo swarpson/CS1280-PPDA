d320040e032e6a1870ae37866115ce4351029695
Swaroop R <swarooprbca24@rvu.edu.in>
2025-01-27 14:57:44 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,5 @@
+x=int(input("Give some number:"))
+if x>0:
+    print("number is Positive")
+else:
+    print("Number is Negative")
---
+++ test3.py
@@ -0,0 +1,8 @@
+x=int(input("give some number:"))
+if x>0:
+    print("Number is positive")
+elif x<0:
+    print("Number is Negative")
+else:
+    print("Number is zero")
+    
