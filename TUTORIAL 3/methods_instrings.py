62e72c039267e1b6bf7fabe3f3ba8aa7b8608a5e
Swaroop R <swarooprbca24@rvu.edu.in>
2025-02-05 15:45:08 +0530

    Delete TUTORIAL 3 directory

--- TUTORIAL 3/methods_instrings.py
+++
@@ -1,8 +0,0 @@
-name=" I am learning python "
-print(name.upper())
-print(name.lower())
-print(name.strip())
-print(name.lstrip())
-print(name.rstrip())
-print(name.replace("python","data science"))
-
