56cd0d8c72a40945b1926388ce11ce7e8b84bdbc
Swaroop R <swarooprbca24@rvu.edu.in>
2025-04-02 15:33:27 +0530

    Add files via upload

---
+++ TUTORIAL 9/P2_A.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+num = int(input("Enter a number to find its factorial:"))
+print(f"The factorial of {num} is {factorial(num)}")
